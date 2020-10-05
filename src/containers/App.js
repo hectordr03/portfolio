a9db0cec4cae1c62a5361ce48ca37039616a1bb3
Hectordr03 <Hectordr03@gmail.com>
2020-10-05 14:18:37 -0400

    housekeeping

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+	return (
+		// Had to 
+		<div className={classes.App}>
+			<h1>Portfolio site</h1>
+      {/* Side nav bar */}
+      {/* <Nav /> */}
+		</div>
+	);
+}
+
+export default App;
